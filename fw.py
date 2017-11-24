11e47da884a3971611680305f832f053cb71233c
Rumen Kolev <Rumen@rumens-macbook-pro-2.local>
2017-11-23 21:23:22 -0700

    Final commit, blank lines and comments are now counted. Firewall is correctly filtering packets

--- fw.py
+++ fw.py
@@ -1,3 +1,9 @@
+# Authors: Rumen Kasabov, Michael Radke
+#
+# Firewall program that takes in a configuration file containing rules and filters out packets based on those rules.
+#
+
+
 import sys
 import os.path
 
@@ -20,182 +26,192 @@ def get_configuration(config_file):
 
         for line in file:
 
-            configuration_rule = line.strip()
-
-            if not configuration_rule.startswith("#"):
+            # Make sure the line is not blank
+            if line.isspace():
+                configuration_lines.append(" ")
+            else:
+                configuration_rule = line.strip()
 
                 configuration_lines.append(configuration_rule)
 
     file.close()
 
     counter = 0
+
     # Loop through config rules and create an array containing a dictionary for each of the 4 or 5 rules for each line
     while counter < len(configuration_lines):
 
-        line_rule = configuration_lines[counter]
-        line_rule = line_rule.split()
-
-        if not (len(line_rule) == 4 or len(line_rule) == 5):
-            print("Error: line " + str(counter) + " contains an incorrect amount of rules")
-            quit()
+        # Add an empty dictionary when there is a blank line or comment to keep track of lines
+        if configuration_lines[counter].startswith("#") or (configuration_lines[counter] == " "):
+            rule_dictionary = {'direction': None , 'action': None, 'min-ip': None, 'max-ip': None,
+                               'ports': None, 'flag': None}
+            rules.append(rule_dictionary)
 
         else:
+            line_rule = configuration_lines[counter]
+            line_rule = line_rule.split()
 
-            direction = line_rule[0]
-            action = line_rule[1]
-            ip_address = line_rule[2]
-            ports = line_rule[3]
-
-            # Split port numbers (if any)
-            ports = ports.split(",")
-
-            if len(line_rule) == 5:
-                flag = line_rule[4]
-
-                # Check if the flag is valid
-                if flag != "established":
-                    print("Line number " + str(counter + 1) + " contains an invalid flag: " + flag)
-                    quit()
-
-            try:
-                # Split ip address and routing prefix
-                ip_address = ip_address.split("/")
-
-                # If a non wildcard address' length is 1 there is no routing prefix
-                if len(ip_address) == 1:
-
-                    if ip_address[0] != "*":
-
-                        # Split each 8 bits of address
-                        ip_address[0] = ip_address[0].split(".")
-
-                        if len(ip_address[0]) == 4:
-
-                            for ip in ip_address[0]:
-
-                                # Check that each 8 bits of the address are within the correct range
-                                if int(ip) < 0 or int(ip) > 255:
-                                    print("Invalid ip address range has been provided on line " + str(counter + 1))
-                                    quit()
-
-                                # Otherwise we convert each 8 bits of address into binary
-                                else:
-
-                                    # The splice [2:] below gets rid of 0b created to indicate binary in python
-                                    binary_address += str(bin(int(ip)))[2:].zfill(8)
-
-                            # Set the minimum and maximum values of the ip address
-                            # In this case, the min and max is the same because the routing prefix is all 32 bytes
-                            min_ip = binary_address
-                            max_ip = binary_address
-
-                            # Re-initialize to 0
-                            binary_address = ""
-
-                        else:
-                            print("Invalid ip address size provided on line " + str(counter + 1))
-                            quit()
-
-                    # If a wildcard is used, we do not change the min and max values for an ip address
-                    elif ip_address[0] == "*":
-                        min_ip = "".ljust(32, '0')
-                        max_ip = "".ljust(32, '1')
-
-                # Otherwise there is an routing prefix
-                elif len(ip_address) == 2:
-
-                    routing_prefix = ip_address[1]
-
-                    # Check if the routing prefix is within correct range
-                    if int(routing_prefix) < 0 or int(routing_prefix) > 32:
-                        print("Invalid routing prefix provided on ip address on line " + str(counter + 1))
-                        quit()
-
-                    else:
-
-                        # Split each 8 bits of address
-                        ip_address[0] = ip_address[0].split(".")
-
-                        if len(ip_address[0]) == 4:
-
-                            for ip in ip_address[0]:
-
-                                # Check that each 8 bits of the address are within the correct range
-                                if int(ip) < 0 or int(ip) > 255:
-                                    print("Invalid ip address range has been provided on line " + str(counter + 1))
-                                    quit()
-
-                                # Otherwise we convert each 8 bits of address into binary
-                                else:
-
-                                    # The splice [2:] below gets rid of 0b created to indicate binary in python
-                                    binary_address += str(bin(int(ip)))[2:].zfill(8)
-
-                            # Set the minimum and maximum values for the 32 bit ip address based on the provided
-                            # routing prefix
-                            # min-ip will set everything after the leading bits in ip address to 0's (min value)
-                            # based on the prefix
-                            min_ip = binary_address[:int(routing_prefix)].ljust(32, '0')
-
-                            # max-ip will set everything after the leadings bits in ip address to 1's (max value)
-                            # based on the prefix
-                            max_ip = binary_address[:int(routing_prefix)].ljust(32, '1')
-
-                            binary_address = ""
-
-
-                        else:
-                            print("Invalid ip address size provided on line " + str(counter + 1))
-                            quit()
-
-            except Exception as e:
-                print("Invalid ip address number on line " + str(counter + 1))
-                print(e)
+            if not (len(line_rule) == 4 or len(line_rule) == 5):
+                print("Error: line " + str(counter) + " contains an incorrect amount of rules")
                 quit()
-
-            if not (direction == "in" or direction == "out"):
-
-                print("Line number " + str(counter + 1) + " contains an invalid direction: " + direction)
-                quit()
-
-            elif not (action == "accept" or action == "drop" or action == "reject"):
-
-                print("Line number " + str(counter + 1) + " contains an invalid action: " + action)
-                quit()
-
-            # Try catch block in case configuration file has a non-integer provided as a port rule
-            try:
-
-                for port in ports:
-
-                    # Check for wildcard
-                    if port == "*":
-                        pass
-
-                    # Check for valid port range
-                    elif int(port) < 0 or int(port) > 65535:
-
-                        print("Line number " + str(counter + 1) + " contains an invalid port range: " + ports)
-                        quit()
-
-            except Exception as e:
-
-                print("Invalid port number on line " + str(counter + 1))
-                print(e)
-                quit()
-
-            if len(line_rule) == 5:
-
-                flag_boolean = "1"
-                rule_dictionary = {'direction': direction, 'action': action, 'min-ip': min_ip, 'max-ip': max_ip,
-                                   'ports': ports, 'flag': flag_boolean}
 
             else:
-                rule_dictionary = {'direction': direction, 'action': action, 'min-ip': min_ip, 'max-ip': max_ip,
-                                   'ports': ports, 'flag': None}
 
-            # Put dictionary in rules array
-            rules.append(rule_dictionary)
+                direction = line_rule[0]
+                action = line_rule[1]
+                ip_address = line_rule[2]
+                ports = line_rule[3]
+
+                # Split port numbers (if any)
+                ports = ports.split(",")
+
+                if len(line_rule) == 5:
+                    flag = line_rule[4]
+
+                    # Check if the flag is valid
+                    if flag != "established":
+                        print("Line number " + str(counter + 1) + " contains an invalid flag: " + flag)
+                        quit()
+
+                try:
+                    # Split ip address and routing prefix
+                    ip_address = ip_address.split("/")
+
+                    # If a non wildcard address' length is 1 there is no routing prefix
+                    if len(ip_address) == 1:
+
+                        if ip_address[0] != "*":
+
+                            # Split each 8 bits of address
+                            ip_address[0] = ip_address[0].split(".")
+
+                            if len(ip_address[0]) == 4:
+
+                                for ip in ip_address[0]:
+
+                                    # Check that each 8 bits of the address are within the correct range
+                                    if int(ip) < 0 or int(ip) > 255:
+                                        print("Invalid ip address range has been provided on line " + str(counter + 1))
+                                        quit()
+
+                                    # Otherwise we convert each 8 bits of address into binary
+                                    else:
+
+                                        # The splice [2:] below gets rid of 0b created to indicate binary in python
+                                        binary_address += str(bin(int(ip)))[2:].zfill(8)
+
+                                # Set the minimum and maximum values of the ip address
+                                # In this case, the min and max is the same because the routing prefix is all 32 bytes
+                                min_ip = binary_address
+                                max_ip = binary_address
+
+                                # Re-initialize to 0
+                                binary_address = ""
+
+                            else:
+                                print("Invalid ip address size provided on line " + str(counter + 1))
+                                quit()
+
+                        # If a wildcard is used, we do not change the min and max values for an ip address
+                        elif ip_address[0] == "*":
+                            min_ip = "".ljust(32, '0')
+                            max_ip = "".ljust(32, '1')
+
+                    # Otherwise there is an routing prefix
+                    elif len(ip_address) == 2:
+
+                        routing_prefix = ip_address[1]
+
+                        # Check if the routing prefix is within correct range
+                        if int(routing_prefix) < 0 or int(routing_prefix) > 32:
+                            print("Invalid routing prefix provided on ip address on line " + str(counter + 1))
+                            quit()
+
+                        else:
+
+                            # Split each 8 bits of address
+                            ip_address[0] = ip_address[0].split(".")
+
+                            if len(ip_address[0]) == 4:
+
+                                for ip in ip_address[0]:
+
+                                    # Check that each 8 bits of the address are within the correct range
+                                    if int(ip) < 0 or int(ip) > 255:
+                                        print("Invalid ip address range has been provided on line " + str(counter + 1))
+                                        quit()
+
+                                    # Otherwise we convert each 8 bits of address into binary
+                                    else:
+
+                                        # The splice [2:] below gets rid of 0b created to indicate binary in python
+                                        binary_address += str(bin(int(ip)))[2:].zfill(8)
+
+                                # Set the minimum and maximum values for the 32 bit ip address based on the provided
+                                # routing prefix
+                                # min-ip will set everything after the leading bits in ip address to 0's (min value)
+                                # based on the prefix
+                                min_ip = binary_address[:int(routing_prefix)].ljust(32, '0')
+
+                                # max-ip will set everything after the leadings bits in ip address to 1's (max value)
+                                # based on the prefix
+                                max_ip = binary_address[:int(routing_prefix)].ljust(32, '1')
+
+                                binary_address = ""
+
+
+                            else:
+                                print("Invalid ip address size provided on line " + str(counter + 1))
+                                quit()
+
+                except Exception as e:
+                    print("Invalid ip address number on line " + str(counter + 1))
+                    print(e)
+                    quit()
+
+                if not (direction == "in" or direction == "out"):
+
+                    print("Line number " + str(counter + 1) + " contains an invalid direction: " + direction)
+                    quit()
+
+                elif not (action == "accept" or action == "drop" or action == "reject"):
+
+                    print("Line number " + str(counter + 1) + " contains an invalid action: " + action)
+                    quit()
+
+                # Try catch block in case configuration file has a non-integer provided as a port rule
+                try:
+
+                    for port in ports:
+
+                        # Check for wildcard
+                        if port == "*":
+                            pass
+
+                        # Check for valid port range
+                        elif int(port) < 0 or int(port) > 65535:
+
+                            print("Line number " + str(counter + 1) + " contains an invalid port range: " + ports)
+                            quit()
+
+                except Exception as e:
+
+                    print("Invalid port number on line " + str(counter + 1))
+                    print(e)
+                    quit()
+
+                if len(line_rule) == 5:
+
+                    flag_boolean = "1"
+                    rule_dictionary = {'direction': direction, 'action': action, 'min-ip': min_ip, 'max-ip': max_ip,
+                                       'ports': ports, 'flag': flag_boolean}
+
+                else:
+                    rule_dictionary = {'direction': direction, 'action': action, 'min-ip': min_ip, 'max-ip': max_ip,
+                                       'ports': ports, 'flag': None}
+
+                # Put dictionary in rules array
+                rules.append(rule_dictionary)
 
         counter += 1
 
@@ -329,7 +345,7 @@ def filter_packets(rules, packets):
                         if rule['flag'] == "1" and packet['flag'] == "1":
 
                             print(rule['action'] + "(" + str(rule_line_number + 1) + ") " + packet['direction'] + " "
-                                  + packet['decimal-address'] + " " +  packet['port'] + " " + packet['flag'])
+                                  + packet['decimal-address'] + " " + packet['port'] + " " + packet['flag'])
 
                             break
 
